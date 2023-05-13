623cfd79099e5bda943725d57dafb62b2f5173b3
Michael Chang <michaelchang.kuo@gmail.com>
2023-05-13 16:15:08 -0700

    palindrome good

--- palindrome.js
+++ palindrome.js
@@ -2,10 +2,6 @@ function palindrome(gg){
   const abc = gg.split("")
 
   let pp = true;
-  if(abc.length % 2 !== 0){
-    console.log('already fail')
-    pp = false;
-  }
   for(let i =0; i < abc.length; i++){
     console.log('sdfsaf')
     console.log('000---',abc[i], abc[(abc.length-1)-i])
@@ -17,4 +13,31 @@ function palindrome(gg){
 
 }
 
-console.log(palindrome('mdodm'));
+console.log(palindrome('mdooodm'));
+
+function pp2(str){
+  let lower = str.toLowerCase();
+  console.log(lower)
+  let left = 0;
+  let right = lower.length-1;
+  while(left < right){
+    console.log(left, right);
+    if(lower[left] !== lower[right]){
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+  return true;
+}
+
+// console.log(pp2('poxon'))
+
+function xd1(abc){
+  let reverse = abc.split("").reverse().join("")
+  console.log(reverse)
+  return reverse.toLowerCase() === abc.toLowerCase();
+}
+
+// console.log(xd1('poxop'))
