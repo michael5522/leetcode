3fdfce787fa9fa6ec2c92c33329e699a9f1a7362
Michael Chang <michaelchang.kuo@gmail.com>
2023-05-20 18:30:30 -0700

    o of 1

---
+++ bigO1.js
@@ -0,0 +1,11 @@
+function linearFunc(arr){
+  for(let i =0; i < arr.length; i++){
+    console.log(1000*100);
+    let something = (50 * 20)/ 5;
+    console.log(something);
+  }
+}
+
+linearFunc([1,2,3,4,5]);
+
+//O of 1
