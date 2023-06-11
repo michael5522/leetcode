fd677a7f51949f2e922dd91600ade762e14b44ee
Michael Chang <michaelchang.kuo@gmail.com>
2023-06-11 16:41:40 -0700

    handle click

--- react-series/01tut/src/Content.js
+++ react-series/01tut/src/Content.js
@@ -5,10 +5,26 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   }
+  const handleClick = () => {
+    console.log(' you clicked it!')
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`)
+  }
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
+  }
 
   return (
     <main>
-      Hello {handleNameChange()}!
+      <p onDoubleClick={handleClick}>
+        Hello {handleNameChange()}!
+      </p>
+      <button onClick={handleClick}> Click it</button>
+      <button onClick={() => {handleClick2('Michael')}}> Click it</button>
+      <button onClick={(e) => { handleClick3(e) }}> Click it</button>
     </main>
   )
 }
