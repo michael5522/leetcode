175fd16ab7a0f91f4ffd512cc85d541185bf2996
Michael Chang <michaelchang.kuo@gmail.com>
2023-06-11 18:55:24 -0700

    props and props drilling

--- react-series/01tut/src/Header.js
+++ react-series/01tut/src/Header.js
@@ -1,14 +1,15 @@
-const Header = () => {
-  // const headerSTyle = {
-  //   backgroundColor: 'royalblue',
-  //   color: '#fff'
-  // }
+const Header = ({title}) => {
+
   return (
     <header>
 
-      <h1> Groceries List</h1>
+      <h1> {title}</h1>
     </header>
   )
 }
 
+
+Header.defaultProps = {
+  title: "Default TItle"
+}
 export default Header
