493f05dac230749567d29ed60ba5a83b738f9e3e
Michael Chang <michaelchang.kuo@gmail.com>
2023-06-14 17:42:32 -0700

    promise cycle

--- ice-cream-asyn-await.js
+++ ice-cream-asyn-await.js
@@ -1,35 +1,36 @@
-const myPromise = new Promise((resolve, reject) => {
-  let food = true;
-  if (food) {
-    setTimeout(()=>{
-      resolve('ola fatty');
-    },3000)
-  } else {
-    reject('ola skinny boy');
-  }
-});
-
-myPromise.then((xxx) => console.log(xxx))
-  .catch((xxx) => console.log(xxx));
-
-// let stocks = {
-//   fruits: ["strawberry", "grapes", "banana", "apple"],
-//   liquid: ["water", "ice"],
-//   holder : ["cone", "cup", "stick"],
-//   topping: ["chocolate", "peanuts"]
-// };
-
-// let is_shop_open = true;
-// console.log(is_shop_open);
-// function time (ms){
-//   return new Promise(resolve, reject) => {
-//     if(is_shop_open){
-//       resolve('promise is met,' ms)
-//     }
-//     reject('its closed')
+// const myPromise = new Promise((resolve, reject) => {
+//   let food = true;
+//   if (food) {
+//     setTimeout(()=>{
+//       resolve('ola fatty');
+//     },3000)
+//   } else {
+//     reject('ola skinny boy');
 //   }
-// }.then((xxx)=>{
-//   console.log(xxx)
-// }).catch(xxx) => {
-//   console.log(xxx)
-// }
+// });
+
+// myPromise.then((xxx) => console.log(xxx))
+//   .catch((xxx) => console.log(xxx));
+
+let stocks = {
+  fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder : ["cone", "cup", "stick"],
+  topping: ["chocolate", "peanuts"]
+};
+
+let is_shop_open = true;
+
+let order = (time,work) => {
+  return new Promise ((resolve,reject) => {
+    if(is_shop_open){
+      setTimeout(()=>{
+        resolve(work())
+      }, time);
+    } else {
+      reject(console.log("out shop is closed"));
+    }
+  });
+};
+
+order(2000, ()=>console.log(`${stocks.fruits[0]} was selected`))
