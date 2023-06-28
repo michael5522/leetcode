086bd3c1fefa46680a39b26af7d18972286c7bbf
Michael Chang <michaelchang.kuo@gmail.com>
2023-06-27 18:02:19 -0700

    react router

--- react-series/04tut/src/App.js
+++ react-series/04tut/src/App.js
@@ -4,12 +4,13 @@ import Home from './Home';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 
-
 function App() {
   return (
     <BrowserRouter>
+    <nav>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
+    </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
