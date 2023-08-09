b99cac8282133eb00eebc122837cef5e8acec449
Michael Chang <michaelchang.kuo@gmail.com>
2023-08-09 16:56:12 -0700

    added event listener on btn

--- javascript-firebase/index.js
+++ javascript-firebase/index.js
@@ -3,3 +3,13 @@ Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
 
+
+
+const cart = document.querySelector("#add-button")
+console.log(cart)
+const cartInput = document.querySelector("#input-field")
+console.log(cartInput.value)
+
+cart.addEventListener("click", function (){
+  console.log(cartInput.value)
+})
