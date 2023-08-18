0cf5bbcb5fe26a58c953c197583c8c1c8c118196
Michael Chang <michaelchang.kuo@gmail.com>
2023-08-17 18:34:55 -0700

    practice

--- es-6/main.js
+++ es-6/main.js
@@ -41,3 +41,20 @@ const fried = (food = "something")=>{
 }
 
 console.log(fried("chicken"));
+
+/*
+    ** includes() Challenge
+        * You want to make a chocolate cake
+        * And you have a list of ingredients represented with an array
+        * Using the JavaScript includes() function
+        * Check if you have the ingredient chocolate in your list of ingredients, and print into the console "We are going to make a chocolate cake" if you have it
+        * Else print into the console "We can't make a chocolate cake because we are missing the ingredient chocolate"
+*/
+
+const listIngredients = ["flour", "sugar", "eggs", "butter"];
+const check = (xxx) => {
+  let boo = xxx.includes("chocolate")
+  console.log(boo)
+  return boo ? `we are goign to make choco cake` : `we cannot make choco cake missing choco`
+}
+console.log(check(listIngredients))
