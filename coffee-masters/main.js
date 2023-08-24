b74a53fefa6a588b952948c7c8544220fee0e18a
Michael Chang <michaelchang.kuo@gmail.com>
2023-08-23 23:55:26 -0700

    js

---
+++ coffee-masters/main.js
@@ -0,0 +1,17 @@
+// let friendsAtYourParty = 0;
+// let count = 0
+// while (friendsAtYourParty < 5) {
+//   count++;
+//   console.log(count)
+//   friendsAtYourParty = friendsAtYourParty + 1;
+// }
+// console.log('xxx',friendsAtYourParty);
+
+// const timetoRepeat= 10;
+// const character = 'x';
+// let answer = ""
+// for(let i = 0; i < 10; i++){
+
+//   answer = answer + character;
+// }
+// console.log(answer);
