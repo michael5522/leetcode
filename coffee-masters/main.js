c71de61592247baa8623c95f27a7ed684aaabefe
Michael Chang <michaelchang.kuo@gmail.com>
2023-08-29 14:55:12 -0700

    js

--- coffee-masters/main.js
+++ coffee-masters/main.js
@@ -1,12 +1,38 @@
 console.log('ola')
-
-let num1 = null;
+let answer = null;
 
 let gg = document.querySelector(".container");
+let display = document.querySelector(".blank")
 // console.log(gg)
 gg.addEventListener("click", function(e){
-  console.log(typeof e.target.innerText)
-  num1 = e.target.innerText;
-  console.log( num1);
+
+  buttonClick(e.target.innerText)
 
 })
+
+function buttonClick(value){
+  if(isNaN(parseInt(value))){
+    handleSymbol(value);
+  }else{
+    handleNumber(value)
+  }
+  displayNum()
+}
+let buffer = '0'
+function handleNumber(value){
+  console.log('handle num',value)
+  if(buffer === '0'){
+    buffer = value
+  }else{
+    buffer+=value;
+  }
+  console.log(buffer)
+}
+
+function handleSymbol(value){
+  console.log('handle symbol',value)
+}
+
+function displayNum(){
+  display.innerText = buffer;
+}
