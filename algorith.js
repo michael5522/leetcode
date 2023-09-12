2bf9879c2af4046cd0abbaeb084a7fde1e4ecd2d
Michael Chang <michaelchang.kuo@gmail.com>
2023-09-12 15:48:22 -0700

    caching

--- algorith.js
+++ algorith.js
@@ -67,17 +67,14 @@ const times10 = (n) => {
   return n * 10
  };
 
-console.log('~~~~~~~~~~~~~~TASK 1~~~~~~~~~~~~~~');
-console.log('times10 returns:', times10(9));
-// Task 2: Use an object to cache the results of your times10 function.
-// protip 1: Create a function that checks if the value for n has been calculated before.
-// protip 2: If the value for n has not been calculated, calculate and then save the result in the cache object.
+// console.log('~~~~~~~~~~~~~~TASK 1~~~~~~~~~~~~~~');
+// console.log('times10 returns:', times10(9));
 
 
 const cache = {};
 const memoTimes10 = (n) => {
   console.log(n)
-
+  console.log(cache)
  if(n in cache){
   console.log('fetch from cache', n);
   return cache[n]
@@ -89,6 +86,6 @@ const memoTimes10 = (n) => {
  }
 }
 
-console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
-console.log('Task 2 calculated value:', memoTimes10(9));	// calculated
-console.log('Task 2 cached value:', memoTimes10(9));	// cached
+// console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
+// console.log('Task 2 calculated value:', memoTimes10(9));
+// console.log('Task 2 cached value:', memoTimes10(9));
