8cec7b637b175379b4819537f2b8c2975ebef90e
Michael Chang <michaelchang.kuo@gmail.com>
2023-09-21 22:07:59 -0700

    css changes

--- react-01/index.js
+++ react-01/index.js
@@ -1,57 +1,3 @@
-function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
-  )
-}
-
-
-function MainContent() {
-  return (
-    <h1>I'm learning React!</h1>
-  )
-}
-
-ReactDOM.render(
-  <div>
-    <Navbar />
-    <MainContent />
-  </div>,
-  document.getElementById("root")
-)
 
 // const root = ReactDOM.createRoot(document.getElementById("root"))
 // root.render(
@@ -60,3 +6,53 @@ ReactDOM.render(
 //     <MainContent/>
 //   </div>
 // )
+
+
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img className="pix" src="https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2023 michael development. All rights reserved.</small>
+    </footer>
+  )
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>It's a popular library, so I'll be
+          able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer
+          if I know React</li>
+      </ol>
+    </div>
+  )
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
