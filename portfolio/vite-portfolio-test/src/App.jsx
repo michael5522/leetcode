8066cc01f91f525ec55760ca23000fe27a97867a
Michael Chang <michaelchang.kuo@gmail.com>
2023-10-10 14:58:38 -0700

    changing colors and getting the id from props

--- portfolio/vite-portfolio-test/src/App.jsx
+++ portfolio/vite-portfolio-test/src/App.jsx
@@ -1,20 +1,39 @@
 
 import Die from './Die.jsx'
 import React from 'react';
+import {nanoid} from "nanoid";
 export default function App() {
+  /**
+   * Challenge: Create a function `holdDice` that takes
+   * `id` as a parameter. For now, just have the function
+   * console.log(id).
+   *
+   * Then, figure out how to pass that function down to each
+   * instance of the Die component so when each one is clicked,
+   * it logs its own unique ID property. (Hint: there's more
+   * than one way to make that work, so just choose whichever
+   * you want)
+   *
+   */
+
 
 
   const [dice, setDice] = React.useState(allNewDice())
   function allNewDice(){
     const array = [];
     for(let i = 0; i < 10; i ++){
-      array.push(Math.floor(Math.random()*6) +1)
+      array.push({value:Math.floor(Math.random()*6) +1, isHeld:false, id: nanoid()})
     }
     return array;
   }
-  const diceElements = dice.map(die=><Die value={die} key={Math.random()}/>)
+  const diceElements = dice.map(die=><Die value={die.value} id={die.id} key={die.id} isHeld={die.isHeld} holdDice={holdDice}/>)
   function reroll(){
     setDice(allNewDice())
+  }
+  console.log(diceElements)
+
+  function holdDice(id){
+    console.log(id)
   }
   return (
     <main>
--- portfolio/vite-portfolio-test/src/Die.jsx
+++ portfolio/vite-portfolio-test/src/Die.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
 
-export default function Die(props){
+export default function Die(props) {
+  console.log(props.holdDice)
+
   return (
-    <div className="die-face">
-      <h2 className="die-num">{props.value}</h2>
+    <div className={`die-face ${props.isHeld ? "savedButton" : ""}`} onClick={() => props.holdDice(props.id)}>
+     <h2 className="die-num">{props.value}</h2>
     </div>
 
   )
