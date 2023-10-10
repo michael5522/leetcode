0ee04afd23a6df0072a2bef363cd696be894f531
Michael Chang <michaelchang.kuo@gmail.com>
2023-10-10 15:51:43 -0700

    added clicking to enable dice to stay

--- portfolio/vite-portfolio-test/src/App.jsx
+++ portfolio/vite-portfolio-test/src/App.jsx
@@ -3,19 +3,6 @@ import Die from './Die.jsx'
 import React from 'react';
 import {nanoid} from "nanoid";
 export default function App() {
-  /**
-   * Challenge: Create a function `holdDice` that takes
-   * `id` as a parameter. For now, just have the function
-   * console.log(id).
-   *
-   * Then, figure out how to pass that function down to each
-   * instance of the Die component so when each one is clicked,
-   * it logs its own unique ID property. (Hint: there's more
-   * than one way to make that work, so just choose whichever
-   * you want)
-   *
-   */
-
 
 
   const [dice, setDice] = React.useState(allNewDice())
@@ -26,14 +13,15 @@ export default function App() {
     }
     return array;
   }
-  const diceElements = dice.map(die=><Die value={die.value} id={die.id} key={die.id} isHeld={die.isHeld} holdDice={holdDice}/>)
+  const diceElements = dice.map(die=><Die value={die.value}  key={die.id} isHeld={die.isHeld} holdDice={holdDice} id={die.id}/>)
   function reroll(){
     setDice(allNewDice())
   }
   console.log(diceElements)
 
   function holdDice(id){
-    console.log(id)
+    const gg = dice.map(die => die.id === id ? {...die, isHeld: !die.isHeld} : die)
+    setDice(gg)
   }
   return (
     <main>
--- portfolio/vite-portfolio-test/src/Die.jsx
+++ portfolio/vite-portfolio-test/src/Die.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 
 export default function Die(props) {
-  console.log(props.holdDice)
+
 
   return (
     <div className={`die-face ${props.isHeld ? "savedButton" : ""}`} onClick={() => props.holdDice(props.id)}>
