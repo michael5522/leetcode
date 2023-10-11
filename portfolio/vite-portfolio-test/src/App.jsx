d3f1638e4499d4876eb7ab9076bad00c9c4fd187
Michael Chang <michaelchang.kuo@gmail.com>
2023-10-10 19:02:24 -0700

    complete app

--- portfolio/vite-portfolio-test/src/App.jsx
+++ portfolio/vite-portfolio-test/src/App.jsx
@@ -6,6 +6,18 @@ export default function App() {
 
 
   const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+
+
+  React.useEffect(()=>{
+      const isHeldArray = []
+      dice.map(die => die.isHeld ? isHeldArray.push(die.value): "");
+      const allEqual = isHeldArray.every(a => a === isHeldArray[0]);
+      if(isHeldArray.length == 10 && allEqual){
+        setTenzies(true)
+        console.log('you win')
+      }
+  },[dice])
   function allNewDice(){
     const array = [];
     for(let i = 0; i < 10; i ++){
@@ -14,21 +26,30 @@ export default function App() {
     return array;
   }
   const diceElements = dice.map(die=><Die value={die.value}  key={die.id} isHeld={die.isHeld} holdDice={holdDice} id={die.id}/>)
+
   function reroll(){
-    setDice(allNewDice())
+    const gg = dice.map(die => die.isHeld === true ? die : { value: Math.floor(Math.random() * 6) + 1, isHeld: false, id: nanoid() })
+    setDice(gg);
+    if(tenzies){
+      setDice(allNewDice())
+      setTenzies(false)
+    }
   }
-  console.log(diceElements)
+
 
   function holdDice(id){
     const gg = dice.map(die => die.id === id ? {...die, isHeld: !die.isHeld} : die)
     setDice(gg)
   }
+
+
+
   return (
     <main>
       <div className="dice-container">
       {diceElements}
       </div>
-      <button className="button-info" onClick={reroll}>Roll</button>
+      <button className="button-info" onClick={reroll}>{tenzies ? "new game" : "roll"}</button>
     </main>
   )
 }
