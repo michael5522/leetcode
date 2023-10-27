32302ebc10db7c048cc06e084347db81f0b511ca
Michael Chang <michaelchang.kuo@gmail.com>
2023-10-26 20:21:46 -0700

    tailwind

--- vite/nike/src/components/ReviewCard.jsx
+++ vite/nike/src/components/ReviewCard.jsx
@@ -3,45 +3,25 @@ import { star } from "../assets/icons";
 const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
   // console.log(imgURL, customerName, rating, feedback)
   return (
-    // <div className='flex justify-center items-center flex-col'>
-    //   <img
-    //     src={imgURL}
-    //     alt='customer'
-    //     className='rounded-full object-cover w-[120px] h-[120px]'
-    //   />
-    //   <p className='mt-6 max-w-sm text-center info-text'>{feedback}</p>
-    //   <div className='mt-3 flex justify-center items-center gap-2.5'>
-    //     <img
-    //       src={star}
-    //       width={24}
-    //       height={24}
-    //       alt='rating star'
-    //       className='object-contain m-0'
-    //     />
-    //     <p className='text-xl font-montserrat text-slate-gray'>({rating})</p>
-    //   </div>
-    //   <h3 className='mt-1 font-palanquin text-3xl text-center font-bold'>
-    //     {customerName}
-    //   </h3>
-    // </div>
-
-
-  <div className="flex flex-col justify-center items-center">
-    <img src={imgURL}
-    className="rounded-full w-[120px] h-[120px] object-cover"
-    />
-    <p className="info-text flex text-center mt-6 max-w-sm">{feedback}</p>
-    <div className="flex">
+    <div className="flex flex-col justify-center items-center">
+      <img
+       src={imgURL}
+        className="rounded-full w-[120px] h-[120px]"
+      />
+      <p className="mt-6 info-text text-center max-w-sm">{feedback}</p>
+      <div className="mt-3 flex justify-center items-center gap-2">
         <img
           src={star}
           width={20}
           height={20}
+          className="object-contain m-0"
         />
-        <p className="info-text">{`(${rating})`}</p>
+        <p className="text-xl mont-montserrat text-slate-gray">({rating})</p>
+      </div>
+      <h3 className="mt-1 font-palanquin text-3xl text-center font-bold">{customerName}</h3>
     </div>
-      <p className="text-2xl">{customerName}</p>
 
-  </div>
+
   );
 };
 
--- vite/nike/src/sections/Footer.jsx
+++ vite/nike/src/sections/Footer.jsx
@@ -5,7 +5,50 @@ import { footerLinks, socialMedia } from "../constants";
 const Footer = () => {
   return (
     <footer className='max-container'>
-     <h1>footer</h1>
+     <div className="flex flex-col">
+    <img
+      src={footerLogo}
+      width="120"
+      height="50"
+    />
+    <p className="text-white flex sm:max-w-sm">Get shoes ready for the new term at your nearest Nike store. Find
+          Your perfect Size In Store. Get Rewards
+    </p>
+
+    <div className="flex">
+      {socialMedia.map((xd)=>(
+        <div className="flex bg-white rounded-full" key={xd.alt}>
+          <img
+            src={xd.src}
+            alt={xd.alt}
+          />
+        </div>
+      ))}
+    </div>
+  </div>
+
+  <div className="flex">
+    {footerLinks.map((xx)=>(
+      <ol className="text-white">
+        {xx.title}
+        {xx.links.map((link)=>(
+          <li>{link.name}</li>
+        ))}
+      </ol>
+    ))}
+  </div>
+
+  <div className="flex justify-between">
+    <div className="flex">
+      <img
+        src={copyrightSign}
+      />
+      <p className="text-white">Copyright. All rights reserved.</p>
+    </div>
+    <div className="flex">
+      <p className="text-white">Terms and conditions</p>
+    </div>
+  </div>
     </footer>
   );
 };
