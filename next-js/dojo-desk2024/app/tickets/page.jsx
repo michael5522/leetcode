a247b65466177faed46047f7447dacd4a548cb08
Michael Chang <michaelchang.kuo@gmail.com>
2024-02-24 18:44:46 -0800

    desk sample

---
+++ next-js/dojo-desk2024/app/tickets/page.jsx
@@ -0,0 +1,20 @@
+import TicketList from "./ticketList";
+import {Suspense} from "react";
+import Loading from './loading';
+
+export default function Tickets(){
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p><small>Currently open tickets</small></p>
+        </div>
+      </nav>
+
+      <Suspense fallback={<Loading/>}>
+        <TicketList />
+      </Suspense>
+    </main>
+  )
+}
